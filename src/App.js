0e146e35a0c381a3e666916807928482ee2fa7be
Marcus Paulo <mpaulobr@gmail.com>
2019-11-26 17:54:07 -0300

    Implementação inicial do Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { Title } from './components/Title/TitleStyle';
-import { Container } from './components/Container/ContainerStyle';
-import { InputText } from './components/InputText/InputTextStyle';
-import { ButtonNext } from './components/ButtonNext/ButtonNextStyle';
-import './App.css';
+import React, { useState } from "react";
+import { Title } from "./components/Title/TitleStyle";
+import { Container } from "./components/Container/ContainerStyle";
+import { InputText } from "./components/InputText/InputTextStyle";
+import { ButtonNext } from "./components/ButtonNext/ButtonNextStyle";
+import { HeaderStyle } from "./components/Header/HeaderStyle";
+import "./App.css";
 
 function App() {
-
   const [nome, setNome] = useState("");
   const [idade, setIdade] = useState("");
   const [escolaridade, setEscolaridade] = useState("");
@@ -14,14 +14,16 @@ function App() {
 
   return (
     <div className="main-container">
+      <HeaderStyle>
+        <Title>Minha Carreira Tech</Title>
+      </HeaderStyle>
       <Container>
-        <Title>
-          Qual seu nome?
-        </Title>
-        <InputText value={nome} onChange={(event) => setNome(event.target.value)}/>
-        <ButtonNext>
-          >
-        </ButtonNext>
+        <Title>Qual seu nome?</Title>
+        <InputText
+          value={nome}
+          onChange={event => setNome(event.target.value)}
+        />
+        <ButtonNext>></ButtonNext>
       </Container>
     </div>
   );
---
+++ src/components/Header/HeaderStyle.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const HeaderStyle = styled.div`
+  background: #17151e;
+  height: 20vh;
+  padding: 0 20px;
+  margin-top: 0;
+`;
